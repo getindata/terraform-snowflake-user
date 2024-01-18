ca207134665b156023d10216ff6b499976fae2be
Dominik Gniewek-Węgrzyn <dominik.gniewek@getindata.com>
2024-01-18 14:27:20 +0100

    chore: modify outputs

--- outputs.tf
+++ outputs.tf
@@ -1,51 +1,51 @@
 output "default_namespace" {
   description = "Specifies the namespace (database only or database and schema) that is active by default for the user's session upon login"
-  value       = one(snowflake_user.this[*].default_namespace)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].default_namespace) : one(snowflake_user.defaults_not_enforced[*].default_namespace)
 }
 
 output "default_role" {
   description = "Specifies the role that is active by default for the user's session upon login"
-  value       = one(snowflake_user.this[*].default_role)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].default_role) : one(snowflake_user.defaults_not_enforced[*].default_role)
 }
 
 output "default_warehouse" {
   description = "Specifies the virtual warehouse that is active by default for the user's session upon login"
-  value       = one(snowflake_user.this[*].default_warehouse)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].default_warehouse) : one(snowflake_user.defaults_not_enforced[*].default_warehouse)
 }
 
 output "disabled" {
   description = "Whether user account is disabled"
-  value       = one(snowflake_user.this[*].disabled)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].disabled) : one(snowflake_user.defaults_not_enforced[*].disabled)
 }
 
 output "login_name" {
   description = "The name users use to log in"
-  value       = one(snowflake_user.this[*].login_name)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].login_name) : one(snowflake_user.defaults_not_enforced[*].login_name)
 }
 
 output "name" {
   description = "Name of the user"
-  value       = one(snowflake_user.this[*].name)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].name) : one(snowflake_user.defaults_not_enforced[*].name)
 }
 
 output "display_name" {
   description = "Name displayed for the user in the Snowflake web interface"
-  value       = one(snowflake_user.this[*].display_name)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].display_name) : one(snowflake_user.defaults_not_enforced[*].display_name)
 }
 
 output "first_name" {
   description = "First name of the user"
-  value       = one(snowflake_user.this[*].first_name)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].first_name) : one(snowflake_user.defaults_not_enforced[*].first_name)
 }
 
 output "last_name" {
   description = "Last name of the user"
-  value       = one(snowflake_user.this[*].last_name)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].last_name) : one(snowflake_user.defaults_not_enforced[*].last_name)
 }
 
 output "email" {
   description = "Email address for the user"
-  value       = one(snowflake_user.this[*].email)
+  value       = var.enforce_defaults ? one(snowflake_user.this[*].email) : one(snowflake_user.defaults_not_enforced[*].email)
 }
 
 output "password" {
